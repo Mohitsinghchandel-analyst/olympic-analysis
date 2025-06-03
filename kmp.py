f693662b4b300f6799b2cf9b0e9d8ec86bdd6d9b
Mohit Singh Chandel <mohitsinghchandel25@gmail.com>
2025-06-04 00:34:20 +0530

    Fix: added CSV and updated path for deployment

--- kmp.py
+++ kmp.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as  plt
 import seaborn as sns
 import plotly.figure_factory as ff
 
-df=pd.read_csv(r'/Users/mohitsinghchandel/Olympic  data analysis/athlete_events.csv')
-df_region=pd.read_csv(r'/Users/mohitsinghchandel/Olympic  data analysis/noc_regions.csv')
+df=pd.read_csv(r'athlete_events.csv')
+df_region=pd.read_csv(r'noc_regions.csv')
 
 df= preprocessor.preprocess(df,df_region)
 
